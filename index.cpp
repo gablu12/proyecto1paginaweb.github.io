e493e3d11e51f67396a3b45f1d2eb3cc50f61f10
gablu12 <gab.lu1258@gmail.com>
2024-11-15 23:39:30 -0400

    cprincipal

---
+++ index.cpp
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+
+<html lang="es"> 
+
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Business Tecnology</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400&family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+<script src="https://kit.fontawesome.com/9b8103ef2f.js" crossorigin="anonymous"></script>
+</head>
+<body>
+ <header class="principal" id="Inicio">
+     <div class="contenido">
+     <div class="logo">
+                <h2>Business <span>Tecnology</span></h2>
+    </div>
+        <nav class="navegacion ocultar">
+         <a href="Inicio">Inicio</a>
+         <a href="Nosotros">Nosotros</a>
+         <a href="Servicios">Servicios</a>
+         <a href="Contacto">Contacto</a>
+            </nav>
+            <div class="hamburguesa">
+                <span></span><span></span><span></span>
+            </div>
+        </div>
+
+        <div class="contenido-encabezado contenedor">
+            <div class="texto-encabezado">
+                <h1>Estamos a la vanguardia de la tecnologia</h1>
+            </div>
+
+            <form class="formulario-encabezado">
+                <h2>Agenda una cita con nosotros</h2>
+                <div class="input-encabezado">
+                    <input type="text" placeholder="Nombre">
+                </div>
+                <div class="input-encabezado">
+                    <input type="mail" placeholder="Correo">
+                </div>
+                <div class="input-encabezado">
+                    <input type="tel" placeholder="Telefono">
+                </div>
+
+                <div class="boton-encabezado">
+                    <input class="btn azul" type="submit" value="Cotizar">
+                </div>
+
+            </form>
+        </div>
+    </header>
+
+    <div class="ventajas">
+     <div class="contenido-ventajas contenedor">
+        <div class="ventaja">
+          <i class="fas fa-clock"></i>
+         <h3>Entregas en tiempo y forma</h3>
+         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa sit quo voluptatem nostrum quis,
+            voluptatum a. Culpa error eligendi, deleniti aliquam fuga ut harum in numquam dolorum amet possimus
+      </p>
+     </div>
+     <div class="ventaja">
+     <i class="fas fa-paint-brush"></i>
+     <h3>Diseño de interfaces modernas</h3>
+       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa sit quo voluptatem nostrum quis,
+              voluptatum a. Culpa error eligendi, deleniti aliquam fuga ut harum in numquam dolorum amet possimus
+      </p>
+     </div>
+            <div class="ventaja">
+                <i class="fas fa-code"></i>
+                <h3>Software de calidad</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa sit quo voluptatem nostrum quis,
+                    voluptatum a. Culpa error eligendi, deleniti aliquam fuga ut harum in numquam dolorum amet possimus
+                </p>
+            </div>
+        </div>
+    </div>
+
+ <section class="nosotros contenedor" id="nosotros">
+  <h2>Sobre nosotros</h2>
+  <div class="cnosotros">
+  <div class="tnosotros">
+     <h3>Hacemos magia con codigo</h3>
+     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit quam laudantium sed minima consectetur!
+     Sint deleniti officia blanditiis omnis possimus temporibus nihil quos. Molestias a cumque libero
+    adipisci, voluptatum minus?</p>
+     <a href="#" class="btn bordes"> saber más</a>
+     </div>
+  </div>
+ </section>
+
+</html>
